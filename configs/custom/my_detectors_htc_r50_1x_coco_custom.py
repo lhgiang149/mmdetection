65734c31f6d987723c2294e1a218d8936dfe7400
toby <lhgiang149@gmail.com>
2020-10-20 16:56:11 +0900

    remove segmentation

--- configs/custom/my_detectors_htc_r50_1x_coco_custom.py
+++ configs/custom/my_detectors_htc_r50_1x_coco_custom.py
@@ -1,4 +1,8 @@
-_base_ = '../htc/htc_r50_fpn_1x_coco.py'
+_base_ = [
+    '../_base_/models/cascade_rcnn_r50_fpn.py',
+    '../_base_/datasets/coco_detection.py',
+    '../_base_/schedules/schedule_1x.py', '../_base_/default_runtime.py'
+]
 
 model = dict(
     backbone=dict(
@@ -26,6 +30,7 @@ model = dict(
             stage_with_sac=(False, True, True, True),
             pretrained='torchvision://resnet50',
             style='pytorch')))
+            
 dataset_type = 'TobyDataset'
 # data_root = '/home/giang/Desktop/dataset/'
 data_root = '/home/../../data3/giangData/'
@@ -44,7 +49,7 @@ train_pipeline = [
     dict(type='DefaultFormatBundle'),
     dict(
         type='Collect',
-        keys=['img', 'gt_bboxes', 'gt_labels', 'gt_masks', 'gt_semantic_seg']),
+        keys=['img', 'gt_bboxes', 'gt_labels']),
 ]
 test_pipeline = [
     dict(type='LoadImageFromFile'),
