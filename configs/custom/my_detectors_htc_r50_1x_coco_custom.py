616a9e440e4ddd8aa146bfed16f31dae39e872f1
toby <lhgiang149@gmail.com>
2020-10-20 16:51:14 +0900

    add normalize

--- configs/custom/my_detectors_htc_r50_1x_coco_custom.py
+++ configs/custom/my_detectors_htc_r50_1x_coco_custom.py
@@ -29,7 +29,9 @@ model = dict(
 dataset_type = 'TobyDataset'
 # data_root = '/home/giang/Desktop/dataset/'
 data_root = '/home/../../data3/giangData/'
-
+img_norm_cfg = dict(
+    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+    
 train_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(
